d978463a2c218701ad37a5c1df0998c0354b4540
Nozipho <114493231+nozipho97@users.noreply.github.com>
2025-04-29 13:53:10 +0200

    Update

--- LanguageClassExercises/TeamOfTwo/Program.cs
+++ LanguageClassExercises/TeamOfTwo/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿namespace TeamOfTwo
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+           
+            LanguageFunctions languageExcercise = new LanguageFunctions();
+
+            Console.WriteLine();
+        }
+    }
+}
